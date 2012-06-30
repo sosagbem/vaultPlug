d493b5be09c26f4b35e995effc1fea72c7e13ac6
Marvin Arnold <marvinmarnold@gmail.com>
2012-06-30 19:22:36 +0300

    Skeleton added

---
+++ vault/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vault::Application.config.secret_token = 'bd163c8044d6daa6b23785dc238a7be030c1160fbb41ae00fe8c7b0912e433f50f58918a254852ddac470570693d6a89f4b8f8f6507beb2d91279e55ca652cfd'
